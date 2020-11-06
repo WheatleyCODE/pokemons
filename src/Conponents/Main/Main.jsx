3775e4a9d3ca111f311ec05b733fa5dce1649fcc
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-06 12:34:24 +0300

    Add layout main page

--- src/Conponents/Main/Main.jsx
+++ src/Conponents/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import MainPage from '../MainPage/MainPage'
 import './Main.scss'
 
 const Main = () => {
   return (
     <div>
       <Header />
+      <MainPage />
       <Footer />
     </div>
   )
---
+++ src/Conponents/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MainPage.scss'
+
+const MainPage = () => {
+  return (
+    <div className="mainPage">
+      <div className="mainPage-pokemons">
+        pokemons
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
