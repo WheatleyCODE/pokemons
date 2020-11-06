6c6fe1cc9a25ee08f605adc62073ad34f86f1091
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-06 12:06:41 +0300

    Add layout header, add fa icons

--- src/Conponents/Header/Header.jsx
+++ src/Conponents/Header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <div className="header">header</div>
+    <div className="header">
+      <div className="header-panel">
+        <button className="header-panel-nemu" type="button">
+          <i className="fa fa-bars" aria-hidden="true" />
+        </button>
+        <span className="header-panel-logo">Pokemons wrap</span>
+      </div>
+    </div>
   )
 }
 
