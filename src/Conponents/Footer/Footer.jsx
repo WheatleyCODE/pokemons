00c8b151f4449325176063fa7dddaba252ffeb7b
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-06 12:17:23 +0300

    add layout footer

--- src/Conponents/Footer/Footer.jsx
+++ src/Conponents/Footer/Footer.jsx
@@ -3,7 +3,13 @@ import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div className="footer">footer</div>
+    <div className="footer">
+      <div className="footer-panel">
+        <span className="footer-panel-text">Первый заголовок</span>
+        <span className="footer-panel-text">Второй заголовок</span>
+        <span className="footer-panel-text">Третий заголовок</span>
+      </div>
+    </div>
   )
 }
 
