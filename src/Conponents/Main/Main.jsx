352a93ce86b8a889734042580383f65d321e77d1
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-06 10:57:14 +0300

    Add the structure

---
+++ src/Conponents/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import './Main.scss'
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Main
