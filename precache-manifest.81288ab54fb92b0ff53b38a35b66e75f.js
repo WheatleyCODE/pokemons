9bceef011a6406fe5fa4e83110421bd348fdfb40
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2020-11-11 22:07:26 +0300

    Updates

---
+++ precache-manifest.81288ab54fb92b0ff53b38a35b66e75f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41f138659281a7aaeccf88cf2190543",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "c36b0721e96722124547",
+    "url": "/pokemons/static/css/main.e3ea2cca.chunk.css"
+  },
+  {
+    "revision": "04b5943713d0252ad875",
+    "url": "/pokemons/static/js/2.345b10a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemons/static/js/2.345b10a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36b0721e96722124547",
+    "url": "/pokemons/static/js/main.054953ed.chunk.js"
+  },
+  {
+    "revision": "b2629a5c54af193e8c39",
+    "url": "/pokemons/static/js/runtime-main.22858f6c.js"
+  }
+]);
